560973c3ef46bb8b3850a2c469795f09e82567b4
alinakassym <alinakassym@gmail.com>
2014-05-02 14:10:58 +0600

    Repositories/HomeRepository.cs was added

---
+++ MvcApp_task1/MvcApp/Repositories/HomeRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.Web;
+
+namespace MvcApp.Repositories
+{
+    public class HomeRepository : IHomeRepository
+    {
+        private usersDBEntities _context;
+
+        public HomeRepository(usersDBEntities userContext)
+        {
+            this._context = userContext;
+        }
+
+        public List<usertbl> GetUsers()
+        {
+            return _context.User.ToList();
+        }
+
+        public usertbl GetUserByID(int iduser)
+        {
+            return _context.User.Find(iduser);
+        }
+
+        public void InsertUser(usertbl user)
+        {
+            _context.User.Add(user);
+        }
+
+        public void DeleteUser(int iduser)
+        {
+            usertbl user = _context.User.Find(iduser);
+            _context.User.Remove(user);
+        }
+
+        public void UpdateUser(usertbl user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ MvcApp_task1/MvcApp/Repositories/IHomeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcApp.Repositories
+{
+    public interface IHomeRepository : IDisposable
+    {
+        List<usertbl> GetUsers();
+        usertbl GetUserByID(int iduser);
+        void InsertUser(usertbl User);
+        void DeleteUser(int iduser);
+        void UpdateUser(usertbl User);
+        void Save();
+    }
+}
